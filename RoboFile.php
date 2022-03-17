96708d32c5cea2db2b24f2b5db42af90b43bbb27
Malabya <malabya35@gmail.com>
2022-03-18 00:46:50 +0530

    Ignore coding standard for root RoboFile.

--- RoboFile.php
+++ RoboFile.php
@@ -1,5 +1,7 @@
 <?php
 
+// @codingStandardsIgnoreStart
+
 /**
  * This is project's console commands configuration for Robo task runner.
  *
@@ -14,3 +16,5 @@ class RoboFile extends Tasks
 {
     // define public methods as commands
 }
+
+// @codingStandardsIgnoreEnd
